5362c0ed22c02aaeb558b459c3e150ce442ce15b
Gaurav Sharma <gauravsharmaa97@gmail.com>
2020-05-17 23:20:38 +0530

    assistant skill json and cloud function javascript code

---
+++ cloud_function.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+
+function main(params) {
+  return new Promise(function (resolve, reject) {
+
+    let discovery;
+
+    if (params.iam_apikey){
+      discovery = new DiscoveryV1({
+        'iam_apikey': params.iam_apikey, //Such values have been added in service credentials.
+        'url': params.url,
+        'version': '2020-03-10'
+      });
+    }
+    else {
+      discovery = new DiscoveryV1({
+        'username': params.username,
+        'password': params.password,
+        'url': params.url,
+        'version': '2020-03-10'
+      });
+    }
+
+    discovery.query({
+      'environment_id': params.environment_id,
+      'collection_id': params.collection_id,
+      'natural_language_query': params.input,
+      'passages': true,
+      'count': 4,
+      'passages_count': 4
+    }, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
+}
